73125f1426a5e07cb8571fb8909022ba64fa438d
chrissetiana <chrissetiana@gmail.com>
2018-06-22 22:01:23 +0100

    Modify activities

--- app/src/main/java/com/chrissetiana/petsapp/EditorActivity.java
+++ app/src/main/java/com/chrissetiana/petsapp/EditorActivity.java
@@ -1,6 +1,80 @@
 package com.chrissetiana.petsapp;
 
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private EditText name, breed, weight;
+    private Spinner gender;
+    private int genderType = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        name = findViewById(R.id.edit_pet_name);
+        breed = findViewById(R.id.edit_pet_breed);
+        weight = findViewById(R.id.edit_pet_weight);
+        gender = findViewById(R.id.spinner_gender);
+
+        setupSpinner();
+    }
+
+    private void setupSpinner() {
+        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array.array_gender_options, android.R.layout.simple_spinner_item);
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        gender.setAdapter(genderSpinnerAdapter);
+
+        gender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals(getString(R.string.gender_male))) {
+                        genderType = 1;
+                    } else if (selection.equals(getString(R.string.gender_female))) {
+                        genderType = 2;
+                    } else {
+                        genderType = 0;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                genderType = 0;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                return true;
+            case R.id.action_delete:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 } 
