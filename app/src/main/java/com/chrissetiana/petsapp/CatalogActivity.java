7718909e6dc6a45dd71c899e22a0e5b32e6973fb
chrissetiana <chrissetiana@gmail.com>
2018-06-27 00:41:51 +0100

    Add dummy data

--- app/src/main/java/com/chrissetiana/petsapp/CatalogActivity.java
+++ app/src/main/java/com/chrissetiana/petsapp/CatalogActivity.java
@@ -1,11 +1,13 @@
 package com.chrissetiana.petsapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +17,8 @@ import com.chrissetiana.petsapp.data.PetContract.PetEntry;
 import com.chrissetiana.petsapp.data.PetDbHelper;
 
 public class CatalogActivity extends AppCompatActivity {
+
+    private PetDbHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +34,8 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
-        displayDatabaseInfo();
+        helper = new PetDbHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
     }
 
     private void displayDatabaseInfo() {
@@ -46,6 +51,19 @@ public class CatalogActivity extends AppCompatActivity {
         }
     }
 
+    private void insertPet() {
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME, "Toto");
+        values.put(PetEntry.COLUMN_PET_BREED, "Terrier");
+        values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
+        values.put(PetEntry.COLUMN_PET_WEIGHT, 7);
+
+        long newRow = db.insert(PetEntry.TABLE_NAME, null, values);
+        Log.wtf("CatalogActivity", "New row id: " + newRow);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_catalog, menu);
@@ -56,6 +74,8 @@ public class CatalogActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_insert_dummy_data:
+                insertPet();
+                displayDatabaseInfo();
                 return true;
             case R.id.action_delete_all_entries:
                 return true;
