33d8e6d8880ebc57e7c0d7aa8dd8f7a0fb7a9b31
chrissetiana <chrissetiana@gmail.com>
2018-07-02 04:12:08 +0100

    Add URIs to contract

--- app/src/main/java/com/chrissetiana/petsapp/data/PetContract.java
+++ app/src/main/java/com/chrissetiana/petsapp/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.chrissetiana.petsapp.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
@@ -8,7 +9,13 @@ public final class PetContract {
 
     }
 
+    public static final String CONTENT_AUTHORITY = "com.chrissetiana.petsapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     public static final class PetEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public static final String TABLE_NAME = "pets";
 
