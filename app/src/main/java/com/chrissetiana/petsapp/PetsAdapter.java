74f959844b03c794f5ab31dadc835cd1238d5c98
chrissetiana <chrissetiana@gmail.com>
2018-07-04 21:41:56 +0100

    Add cursor adapter

---
+++ app/src/main/java/com/chrissetiana/petsapp/PetsAdapter.java
@@ -0,0 +1,23 @@
+package com.chrissetiana.petsapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+public class PetsAdapter extends CursorAdapter {
+
+    public PetsAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+    }
+}
